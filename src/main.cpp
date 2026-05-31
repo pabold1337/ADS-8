92f50604ca63e518d9e865548a09ea53fb88937f
pabold1337 <apa2007nn@gmail.com>
2026-05-31 20:52:20 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <string>
+
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
 
 int main() {
     BST<std::string> tree;
