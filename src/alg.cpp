d578ee1802f7c9f85cc49669b8817677e557c523
pabold1337 <apa2007nn@gmail.com>
2026-05-31 21:22:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,11 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
+#include "alg.h"
 
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
 #include <cctype>
+#include <fstream>
+#include <iostream>
+#include <vector>
 
 void makeTree(BST<std::string>& tree, const char* filename) {
   std::ifstream file(filename);
