0612d6fe099643cb4eaf85ddbb1d3ddf92a09ae6
pabold1337 <apa2007nn@gmail.com>
2026-05-31 21:24:46 +0300

    Create alg.h

---
+++ include/alg.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_ALG_H_
+#define INCLUDE_ALG_H_
+
+#include "bst.h"
+#include <string>
+
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
+#endif  // INCLUDE_ALG_H_
