6f4763d04ce2a4211e6e1772d22242e6643ce75c
pabold1337 <apa2007nn@gmail.com>
2026-06-10 19:21:04 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -1,12 +1,10 @@
-// Copyright 2021 NNTU-CS
-#ifndef INCLUDE_ALG_H_
-#define INCLUDE_ALG_H_
-
-#include <string>
+#ifndef ALG_H_
+#define ALG_H_
 
 #include "bst.h"
+#include <string>
 
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
-#endif  // INCLUDE_ALG_H_
+#endif  // ALG_H_
