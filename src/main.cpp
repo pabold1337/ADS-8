a5ab3759a7cd2f114bb8d1673c2aca5793461f0c
pabold1337 <apa2007nn@gmail.com>
2026-06-10 19:23:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 // Copyright 2021 NNTU-CS
-#include <string>
 #include "alg.h"
 
 int main() {
-  BST<std::string> tree;
-  makeTree(tree, "src/war_peace.txt");
-  printFreq(tree);
-  return 0;
+    BST<std::string> tree;
+    makeTree(tree, "src/war_peace.txt");
+    printFreq(tree);
+    return 0;
 }
